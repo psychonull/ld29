5f64bc22c83a38dfefd9c63f90b7dbc238fb0f8a
Matias Arriola <matias.arriola@gmail.com>
2014-04-27 04:50:21 -0300

    testing bitmap fonts + audio

--- game/states/menu.js
+++ game/states/menu.js
@@ -7,23 +7,52 @@ Menu.prototype = {
 
   },
   create: function() {
+    this.game.stage.backgroundColor = '#61e1e9';
     var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
-    this.sprite = this.game.add.sprite(this.game.world.centerX, 138, 'yeoman');
+    this.sprite = this.game.add.sprite(this.game.world.centerX, 138, 'cart');
     this.sprite.anchor.setTo(0.5, 0.5);
 
-    this.titleText = this.game.add.text(this.game.world.centerX, 300, '\'Allo, \'Allo!', style);
-    this.titleText.anchor.setTo(0.5, 0.5);
 
-    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, 'Click anywhere to play "Click The Yeoman Logo"', { font: '16px Arial', fill: '#ffffff', align: 'center'});
-    this.instructionsText.anchor.setTo(0.5, 0.5);
+    this.scoreText = this.game.add.bitmapText(0, 300, 'minecraftia','PLAY', 42);
+    this.scoreText.align = 'center';
+    this.scoreText.x = this.game.width / 2 - this.scoreText.textWidth / 2;
+    this.scoreText.y = this.game.height / 2 - this.scoreText.textHeight / 2;
+    
+    this.instructionsText = this.game.add.bitmapText(270, 365, 'minecraftia', 'Use Up/Down Arrows to move', 16);
+    this.instructionsText.x = this.game.width / 2 - this.instructionsText.textWidth / 2;
 
     this.sprite.angle = -20;
-    this.game.add.tween(this.sprite).to({angle: 20}, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
+    this.game.add.tween(this.sprite).to({angle: 20}, 300, Phaser.Easing.Quadratic.InOut, true, 0, 300, true);
+
+    this.game.sound.mute = true;
+    this.audioFx = this.game.add.audio('playSfx');
   },
   update: function() {
     if(this.game.input.activePointer.justPressed()) {
-      this.game.state.start('play');
+      this.pregameGlitch(function(){
+        this.game.state.start('play');
+      });
     }
+  },
+  pregameGlitch: function(done){
+    var colors = ['#61e1e9','#967750','#584635'],
+        timer = this.game.time.create(false),
+        eventCount = 0;
+    this.audioFx.play();
+    var execEvent = function(){
+      this.game.stage.backgroundColor = this.game.rnd.pick(colors);
+      eventCount++;
+      if (eventCount < 15){
+        timer.add(this.game.rnd.integerInRange(50, 150), execEvent, this);
+      }
+      else {
+        timer.stop();
+        done.call(this);
+      }
+    };
+
+    timer.loop(this.game.rnd.integerInRange(50, 150), execEvent, this);
+    timer.start();
   }
 };
 
