510fb6b7a768e89ec865d4d1e833a3dcb82917e1
Pablo Novas <pjnovas@gmail.com>
2014-04-28 13:58:35 -0300

    added gold

--- game/prefabs/ending.js
+++ game/prefabs/ending.js
@@ -5,16 +5,16 @@ var Ending = function(game) {
 
   this.add(this.game.add.image(0, 295, 'end', 'floor_right'));
 
-  this.minigame = this.game.add.sprite(100,200,'cart');
-
-  this.add(this.minigame);
+  this.add(this.game.add.image(100, 300, 'end', 'gold_pile'));
+  //this.minigame = this.game.add.sprite(100,200,'cart');
+  //this.add(this.minigame);
 };
 
 Ending.prototype = Object.create(Phaser.Group.prototype);
 Ending.prototype.constructor = Ending;
 
 Ending.prototype.update = function() {
-  
+
 };
 
 Ending.prototype.start = function(){
