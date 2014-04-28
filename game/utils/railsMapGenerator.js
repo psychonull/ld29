cf6256f16f3005fad1e162b1d03fbde13549782f
Matias Arriola <matias.arriola@gmail.com>
2014-04-28 12:48:49 -0300

    generator difficulty

--- game/utils/railsMapGenerator.js
+++ game/utils/railsMapGenerator.js
@@ -6,8 +6,7 @@ var Rails = require('../prefabs/rails'),
 var game = Phaser.GAMES[0];
 
 var RailsMapGenerator = {
-	height: 4,
-	difficulty: 1,
+	difficulty: 3,
 	startingOffset: 6,
 
 	generate: function(width){
@@ -17,7 +16,7 @@ var RailsMapGenerator = {
 			map[i].push(ObstacleTypes.START_ID);
 			this.addOffset(map[i]);
 			for(var j = 0; j < width; j++){
-				map[i].push(Math.round(Math.random() * 30) === 2 ? game.rnd.pick(obstacles) : 0);
+				map[i].push(Math.round(Math.random() * 50 / this.difficulty) === 0 ? game.rnd.pick(obstacles) : 0);
 			}
 			map[i].push(ObstacleTypes.WIN_ID);
 		}
